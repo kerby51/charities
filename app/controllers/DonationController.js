512e47791467805fab5588c851e745709d097078
katharine benefico <kbenefico@gmail.com>
2016-10-29 11:06:42 -0400

    update charityName to charity

--- app/controllers/DonationController.js
+++ app/controllers/DonationController.js
@@ -10,7 +10,7 @@ class DonationController {
   static create(req, res) {
     const donationData = {
       name: req.body.name,
-      charityName: req.body.charityName,
+      charity: req.body.charity,
       amount: req.body.amount,
     };
     DonationDAO.create(donationData)
